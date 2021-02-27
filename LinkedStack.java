b855f4a95c59bf9bb2b43408e87a2bff56c0d212
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 18:44:10 -0800

    Initialized convertToPostfix - Josh

--- LinkedStack.java
+++ LinkedStack.java
@@ -51,61 +51,6 @@ public class LinkedStack<T> implements StackInterface<T>
         Node newNode = new Node(newEntry, topNode);
         topNode = newNode;
     }
-    
-    public static void convertToPostfix(infix)
-    {
-        operatorStack = STACK.empty();
-        String postfix = new String("");
-        while (true)
-        {
-            nextCharacter = next nonblank character of infix
-            switch (nextCharacter)
-            {
-                case variable:
-                {
-                    Append nextCharacter to postfix
-                    break;
-                }
-                case '^' :
-                {
-                    operatorStack.push(nextCharacter)
-                    break;
-                }
-                case '+' : case '-' : case '*' : case '/' :
-                {
-                    while (!operatorStack.isEmpty() and precedence of nextCharacter <= precedence of operatorStack.peek());
-                    {
-                        Append operatorStack.peek() to postfix
-                        operatorStack.pop()
-                    }
-                    operatorStack.push(nextCharacter)
-                    break;
-                }
-                case '(' :
-                {
-                    operatorStack.push(nextCharacter)
-                    break;
-                }
-                case ')' : // Stack is not empty if infix expression is valid
-                {
-                    topOperator = operatorStack.pop()
-                    while (topOperator != '(')
-                    {
-                        Append topOperator to postfix
-                        topOperator = operatorStack.pop()
-                    }
-                    break;
-                }
-                default: break // Ignore unexpected characters
-            }
-        }
-        while (!operatorStack.isEmpty())
-        {
-            topOperator = operatorStack.pop()
-            Append topOperator to postfix
-        }
-        return postfix
-    }
 
     public T pop()
     {
@@ -130,5 +75,20 @@ public class LinkedStack<T> implements StackInterface<T>
     public void clear()
     {
         topNode = null;
+    }
+
+    public void convertToPostfix(LinkedStack<T> inputStack)
+    {
+        LinkedStack<T> operatorStack = new LinkedStack<T>();
+        LinkedStack<T> valueStack = new LinkedStack<T>();
+        T nextCharacter;
+        while(!inputStack.isEmpty())
+        {
+            nextCharacter = inputStack.peek();
+            switch(nextCharacter)
+            {
+
+            }
+        }
     }
 }
