1d2e79cb5340ec37d205bc3a72a31f06310c8457
rdrickards <78835468+rdrickards@users.noreply.github.com>
2021-03-01 13:09:34 -0800

    adding tests to linkedStackTest - Russell

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -2,6 +2,8 @@ public class LinkedStackTest
 {
     public static void main(String[] args)
     {
-        
+        String infix = "𝑎∗ 𝑏 / (𝑐−𝑎)+𝑑∗𝑒";
+        System.out.println("Infix: " + infix);
+        System.out.println("Postfix: " + convertToPostfix(infix));
     }
 }
